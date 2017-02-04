e64e1dcbfecb8980de115071fbf3a95c5fe38329
foss <foss@DABOL.localnet>
2017-02-04 15:57:45 -0800

    finished first part of hw

--- hw/wk1-thur/6-rain-or-shine/rain-or-shine.cpp
+++ hw/wk1-thur/6-rain-or-shine/rain-or-shine.cpp
@@ -3,42 +3,64 @@
 #include <string>
 
 // const definitions
-const char RAINY = 'R';
-const char CLOUDY = 'C';
-const char SUNNY = 'S';
-
-const int JUNE = 0;
-const int JULY = 1;
-const int AUGUST = 2;
+const char weather[3] = {'R', 'C', 'S'};
+const std::string weather_s[3] = {"Rainy", "Cloudy", "Sunny"};
+const std::string months[3] = {"June", "July", "August"};
 // end const definitions
 
 // function prototypes
 void readData(char[3][30]);
 void printData(char[3][30]);
+int countDays(char[3][30], int, char);
+void weatherReport(char[3][30]);
+// end function prototypes
 
 int main() {
   char data[3][30];
+
   readData(data);
-  printData(data); 
+  // printData(data);
+  weatherReport(data);
+
   return 0;
 }
 
-void printData(char w[3][30]) {
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 30; j++) {
-            std::cout << w[i][j];
-        }
-        std::cout << std::endl;
+void weatherReport(char w[3][30]) {
+  for (int i = 0; i < 3; i++) {
+    std::cout << months[i] << " weather:" << std::endl;
+    std::cout << "Rainy\tCloudy\tSunny" << std::endl;
+    for (int j = 0; j < 3; j++) {
+      std::cout << countDays(w, i, weather[j]) << "\t";
     }
+    std::cout << std::endl << std::endl;
+  }
+}
+
+int countDays(char w[3][30], int month, char weather_type) {
+  int count = 0;
+  for (char c : w[month]) {
+    if (c == weather_type)
+      count++;
+  }
+  return count;
+}
+
+void printData(char w[3][30]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 30; j++) {
+      std::cout << w[i][j];
+    }
+    std::cout << std::endl;
+  }
 }
 
 void readData(char w[3][30]) {
-  std::ifstream inData("RainOrShine.dat");
+  std::ifstream in_data("RainOrShine.dat");
   std::string line;
-  if (inData.is_open()) {
+  if (in_data.is_open()) {
     int month = 0;
-    while (getline(inData, line)) {
-      //std::cout << line << std::endl;
+    while (getline(in_data, line)) {
+      // std::cout << line << std::endl;
       for (int i = 0; i < line.length(); i++) {
         w[month][i] = line[i];
       }
