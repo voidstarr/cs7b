76a345e2892ebff58b28b4701155b5033afa6b45
voidstar <voidstarr@users.noreply.github.com>
2017-02-07 13:09:08 -0800

    yeah

--- lab/2-7-17/programming-challenges/9-lo-shu-magic-square/lo-shu-magic-square.cpp
+++ lab/2-7-17/programming-challenges/9-lo-shu-magic-square/lo-shu-magic-square.cpp
@@ -4,15 +4,10 @@ void printSquare(int[3][3]);
 bool isMagicSquare(int[3][3]);
 void validateMagic(int[3][3]);
 
-
 int main() {
-  int magic[3][3] = {{4, 9, 2}, 
-                     {3, 5, 7}, 
-                     {8, 1, 6}};
-  
-  int notMagic[3][3] = {{4,2,0},
-                        {7,1,0},
-                        {5,5,5}};
+  int magic[3][3] = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
+
+  int notMagic[3][3] = {{4, 2, 0}, {7, 1, 0}, {5, 5, 5}};
 
   validateMagic(magic);
   validateMagic(notMagic);
@@ -21,13 +16,13 @@ int main() {
 }
 
 void validateMagic(int s[3][3]) {
-printSquare(s);
+  printSquare(s);
 
   if (isMagicSquare(s)) {
-          std::cout << "is a magic square!" << std::endl;
-            } else {
-                    std::cout << "is not a magic square. :[" << std::endl;
-                      }
+    std::cout << "is a magic square!" << std::endl;
+  } else {
+    std::cout << "is not a magic square. :[" << std::endl;
+  }
 }
 
 void printSquare(int s[3][3]) {
@@ -38,6 +33,7 @@ void printSquare(int s[3][3]) {
     std::cout << std::endl;
   }
 }
+
 bool isMagicSquare(int s[3][3]) {
   for (int i = 0; i < 3; i++)
     if ((s[i][0] + s[i][1] + s[i][2]) != 15 &&
@@ -46,7 +42,7 @@ bool isMagicSquare(int s[3][3]) {
 
   if ((s[0][0] + s[1][1] + s[2][2]) != 15 &&
       (s[0][2] + s[1][1] + s[2][0]) != 15)
-      return false;
+    return false;
 
   return true;
 }
