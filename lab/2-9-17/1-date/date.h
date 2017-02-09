8d0e719133607ab024fd9c69537773fefba3d6c7
voidstar <voidstarr@users.noreply.github.com>
2017-02-09 12:39:49 -0800

    finished #1 pg499

---
+++ lab/2-9-17/1-date/date.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class Date {
+    private:
+        int day;
+        int month;
+        int year;
+        std::string yrShort;
+        const std::string mNames[12] = {"January","February","March","April",
+                          "May","June","July","August","September",
+                          "October","November","December"};
+    public:
+        Date(int m,int d,int y): day(d), month(m), year(y){
+            yrShort = std::to_string(y);
+            yrShort = yrShort.substr(2,2);
+        };
+        Date(): day(1), month(12), year(2001){};
+        void printDate(int);
+        void monthFirst();
+        void dayFirst();
+        void slashPrint();
+};
