4513ab6cf1e50c45e50c1f5901d8ea967a2c3532
voidstar <voidstarr@users.noreply.github.com>
2017-02-09 15:43:29 -0800

    added user input

--- lab/2-9-17/2-report-heading/main.cpp
+++ lab/2-9-17/2-report-heading/main.cpp
@@ -1,32 +1,53 @@
 #define ONE_LINE 0
 #define FOUR_LINE 1
 
-#include <iostream>
 #include "heading.h"
+#include <iostream>
 
-#include <sys/ioctl.h>
 #include <stdio.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
+Heading createHeading();
 
 int main() {
 
+  // get console width
+  int width;
+  struct winsize w;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  width = w.ws_col;
+  // ---------------------
 
-    // get console width
-    int width;
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    width = w.ws_col;
-    // ---------------------
+  // Heading abc;
+  // abc.setWidth(width);
+  // abc.printReport(ONE_LINE);
+  // abc.printReport(FOUR_LINE);
 
-    Heading abc;
-    abc.setWidth(width);
-    abc.printReport(ONE_LINE);
-    abc.printReport(FOUR_LINE);
+  std::string comp;
+  std::string rep;
+  
+  Heading h = createHeading();
 
-    Heading bigBoss("Big Boss LCC", "Mainframe Status");
-    bigBoss.setWidth(width);
-    bigBoss.printReport(ONE_LINE);
-    bigBoss.printReport(FOUR_LINE);
-    return 0;
+  h.setWidth(width);
+  h.printReport(ONE_LINE);
+  h.printReport(FOUR_LINE);
+  return 0;
+}
+
+Heading createHeading() {
+
+  std::string comp;
+  std::string rep;
+
+  std::cout << "Enter name of company (blank for default): " << std::endl
+            << ">> ";
+  std::getline(std::cin, comp);
+  if (comp.length() != 0) {
+    std::cout << "Enter name of report:" << std::endl << ">> ";
+    std::getline(std::cin, rep);
+    return Heading(comp, rep);
+  } else {
+    return Heading();
+  }
 }
