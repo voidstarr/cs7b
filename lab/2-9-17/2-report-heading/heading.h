bc1ff1c909cde50f8f3f5810d5ac2dca8854f584
voidstar <voidstarr@users.noreply.github.com>
2017-02-09 15:10:24 -0800

    finished #2 pg500

--- lab/2-9-17/2-report-heading/heading.h
+++ lab/2-9-17/2-report-heading/heading.h
@@ -1,13 +1,23 @@
 #pragma once
 #include <string>
+#include <iostream>
+
 
 class Heading {
     public:
         Heading(): companyName("ABC Industries"), reportName("Report") {};
-        Heading(std::string c, std::string r): companyName(c), reportName(r) {};
+        Heading(std::string c, std::string r): companyName(c), reportName(r) {
+        };
         void printReport(int);
+        void setWidth(int);
     private:
+        const std::string stars = "********************************************************";
+
         std::string companyName;
         std::string reportName;
+        int consoleWidth = 10;
 
+        void oneLineReport();
+        void fourLineReport();
+        void centerString(std::string);
 };
--- lab/2-9-17/2-report-heading/main.cpp
+++ lab/2-9-17/2-report-heading/main.cpp
@@ -4,15 +4,28 @@
 #include <iostream>
 #include "heading.h"
 
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 
 int main() {
 
+
+    // get console width
+    int width;
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    width = w.ws_col;
+    // ---------------------
+
     Heading abc;
+    abc.setWidth(width);
     abc.printReport(ONE_LINE);
     abc.printReport(FOUR_LINE);
 
     Heading bigBoss("Big Boss LCC", "Mainframe Status");
+    bigBoss.setWidth(width);
     bigBoss.printReport(ONE_LINE);
     bigBoss.printReport(FOUR_LINE);
     return 0;
