451a3e8994f633d04f5bd0d88341480ac3624cbf
voidstar <voidstarr@users.noreply.github.com>
2017-02-21 13:15:53 -0800

    finished 6

--- lab/2-21-17/6-string-selection-sort/main.cpp
+++ lab/2-21-17/6-string-selection-sort/main.cpp
@@ -5,11 +5,12 @@ void selectionSort(std::string[], int);
 
 int main() {
   const int SIZE = 20;
-  string name[SIZE] = {"Collins, Bill",  "Smith, Bart",  "Michalski, Joe",
-                       "Sanchez, Manny", "Rubin, Sarah", "Taylor, Tyrone",
-                       "Allison, Jeff",  "Moreno, Juan", "Wolfe, Bill",
-                       "Moretti, Bella", "Wu, Hong",     "Patel, Renee",
-                       "Smith, Cathy",   "Conroy, Pat",  "Kelly, Sean"};
+  std::string name[SIZE] = {
+      "Collins, Bill",  "Smith, Bart",  "Michalski, Joe", "Griffin, Jim",
+      "Sanchez, Manny", "Rubin, Sarah", "Taylor, Tyrone", "Johnson, Jill",
+      "Allison, Jeff",  "Moreno, Juan", "Wolfe, Bill",    "Whitman, Jean",
+      "Moretti, Bella", "Wu, Hong",     "Patel, Renee",   "Harrison, Rose",
+      "Smith, Cathy",   "Conroy, Pat",  "Kelly, Sean",    "Holland, Beth"};
   std::cout << "unsorted:" << std::cout;
 
   printArray(name, SIZE);
