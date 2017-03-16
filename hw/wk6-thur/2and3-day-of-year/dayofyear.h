329823c240e86e10d9a602e90d1faa2832237dec
voidstar <voidstarr@users.noreply.github.com>
2017-03-16 12:55:36 -0700

    added book's approach

--- hw/wk6-thur/2and3-day-of-year/dayofyear.h
+++ hw/wk6-thur/2and3-day-of-year/dayofyear.h
@@ -1,48 +1,70 @@
 #pragma once
-#include <iostream>
-#include <stdexcept>
 #include <cctype>
+#include <iostream>
 #include <sstream>
-
+#include <stdexcept>
 
 class DayOfYear {
-    public:
-        DayOfYear(int d) {
-            this->doy = d;
-            convert2MonthAndDay();
-            monthAndDay = false;
-        }
+public:
+  DayOfYear() {
+    this->doy = 0;
+    this->day = 0;
+    this->month = "";
+  }
 
-        DayOfYear(std::string m, int d) {
-            this->month = m;
-            int cm = -1;
-            for(int i = 0; i < 12; i++)
-                if (toLower(m) == toLower(months[i]))
-                    cm = i;
-            if (cm == -1)
-                throw std::invalid_argument("month must be valid!");
-            this->day = d;
-            if (d > daysInMonth[cm])
-                throw std::invalid_argument("day must be within valid range!");
-            convert2DayOfYear();
-            monthAndDay = true;
-        }
+  DayOfYear(int d) {
+    this->doy = d;
+    this->month = "";
+    this->day = 0;
+    convert2MonthAndDay();
+    monthAndDay = false;
+  }
 
-        void print();
-    private:
-        bool monthAndDay;
+  DayOfYear(std::string m, int d, bool myWay) {
+    this->month = m;
+    this->day = d;
+    this->doy = 0;
+    if (myWay) {
+      int cm = -1;
+      for (int i = 0; i < 12; i++)
+        if (toLower(m) == toLower(months[i]))
+          cm = i;
+      if (cm == -1)
+        throw std::invalid_argument("month must be valid!");
+      if (d > daysInMonth[cm])
+        throw std::invalid_argument("day must be within valid range!");
+      convert2DayOfYear();
+    }
+    monthAndDay = true;
+  }
 
-        void convert2MonthAndDay();
-        void convert2DayOfYear();
+  void print();
+  int getDay() { return day; }
+  void setDay(int d) { day = d; }
+  int getDoY() { return doy; }
+  void setDoY(int d) { doy = d; }
+  std::string getMonth() { return month; }
+  void setMonth(std::string m) { month = m; }
 
-        int day;
-        std::string month;
+  static std::string toLower(std::basic_string<char> &);
+  static std::string months[];
+  static int daysInMonth[];
 
-        int doy;
-        static std::string months[];
-        static int daysInMonth[];
-        
-        static std::string toLower(std::basic_string<char>&);
+  DayOfYear operator++(int i) {
+    doy++;
+    if (doy >= 365)
+      doy = 0;
+    return *this;
+  }
 
+private:
+  bool monthAndDay;
 
+  void convert2MonthAndDay();
+  void convert2DayOfYear();
+
+  int day;
+  std::string month;
+
+  int doy;
 };
--- hw/wk6-thur/2and3-day-of-year/main.cpp
+++ hw/wk6-thur/2and3-day-of-year/main.cpp
@@ -3,12 +3,14 @@
 
 void option1();
 void option2();
+void option3();
 
 int main() {
   do {
     std::cout << "Enter an option: " << std::endl
               << "1: Day of Year -> Month and Day" << std::endl
-              << "2: Month and Day -> Day of Year" << std::endl
+              << "2: Month and Day -> Day of Year (my way)" << std::endl
+              << "3: Month and Day -> Day of Year (highway)" << std::endl
               << "-1: Quit" << std::endl
               << ">> ";
 
@@ -19,13 +21,60 @@ int main() {
       option1();
     } else if (choice == 2) {
       option2();
+    } else if (choice == 3) {
+      option3();
     } else if (choice < 0) {
-        break;
+      break;
     } else {
       std::cout << "Invalid choice." << std::endl << std::endl;
     }
   } while (true);
   return 0;
+}
+
+void option3() {
+  do {
+    std::cout << "Enter a month:" << std::endl << ">> ";
+    std::string m;
+    std::cin.ignore();
+    std::getline(std::cin, m);
+    int d;
+    std::cout << "Enter a number between 1 and 31 or not to return to menu:"
+              << std::endl
+              << ">> ";
+    std::cin >> d;
+    if (!(d > 0 && d < 32)) {
+      return;
+    }
+
+    bool found = false;
+    for (int i = 0; i < 12; i++)
+      if (DayOfYear::toLower(m) == DayOfYear::toLower(DayOfYear::months[i]))
+        found = true;
+
+    if (!found) {
+      std::cout << "month must be valid" << std::endl;
+      continue;
+    }
+
+    DayOfYear doy(m, d, false);
+
+    int daysSinceNewYear = 0;
+    for (int i = 0; i < 12; i++) {
+      if (DayOfYear::toLower(DayOfYear::months[i]) == DayOfYear::toLower(m)) {
+        for (int j = 0; j < doy.getDay(); j++)
+          doy++;
+        break;
+      } else {
+        for (int j = 0; j < DayOfYear::daysInMonth[i]; j++)
+          doy++;
+      }
+    }
+
+    doy.print();
+
+    std::cout << std::endl << std::endl;
+  } while (true);
 }
 
 void option2() {
@@ -43,7 +92,7 @@ void option2() {
       return;
     }
 
-    DayOfYear doy(m, d);
+    DayOfYear doy(m, d, true);
     doy.print();
 
     std::cout << std::endl << std::endl;
