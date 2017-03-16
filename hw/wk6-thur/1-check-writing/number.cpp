83c84db455fe9013055ce92930bede1d6073b473
voidstar <voidstarr@users.noreply.github.com>
2017-03-16 13:50:43 -0700

    added zero case

--- hw/wk6-thur/1-check-writing/number.cpp
+++ hw/wk6-thur/1-check-writing/number.cpp
@@ -1,44 +1,53 @@
 #include "number.h"
 
-const std::string Number::lessThan20[] = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve",
-                                "thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-const std::string Number::tensPlace[] = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+const std::string Number::lessThan20[] = {
+    "zero",    "one",     "two",       "three",    "four",
+    "five",    "six",     "seven",     "eight",    "nine",
+    "ten",     "eleven",  "twelve",    "thirteen", "fourteen",
+    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+const std::string Number::tensPlace[] = {"twenty", "thirty", "fourty",
+                                         "fifty",  "sixty",  "seventy",
+                                         "eighty", "ninety"};
 const std::string Number::hundred = "hundred";
 const std::string Number::thousand = "thousand";
 
 void Number::print() {
-    int digits[4] = {0,0,0,0};
-    int num = this->number;
-    int i = 3;
-    while (num != 0) {
-        digits[i] = num % 10;
-        num /= 10;
-        i--;
-    }
+  int digits[4] = {0, 0, 0, 0};
+  int num = this->number;
+  int i = 3;
+  while (num != 0) {
+    digits[i] = num % 10;
+    num /= 10;
+    i--;
+  }
 
-    std::cout << std::endl;
+  std::cout << std::endl;
 
-    std::stringstream output;
+  std::stringstream output;
+
+  if (this->number == 0) {
+    output << lessThan20[0];
+  } else {
 
     if (digits[0] > 0) {
-        output << lessThan20[digits[0]] << " " << thousand << " "; 
-    }    
+      output << lessThan20[digits[0]] << " " << thousand << " ";
+    }
 
     if (digits[1] > 0) {
-        output << lessThan20[digits[1]] << " " << hundred << " ";
+      output << lessThan20[digits[1]] << " " << hundred << " ";
     }
-
 
     if (digits[2] == 0 && digits[3] > 0) {
-                output << lessThan20[digits[3]];
+      output << lessThan20[digits[3]];
     } else if (digits[2] == 1) {
-        output << lessThan20[10 + digits[3]];
+      output << lessThan20[10 + digits[3]];
     } else if (digits[2] > 1) {
-        output << tensPlace[digits[2] - 2];
-        if (digits[3] > 0) {
-            output << " " << lessThan20[digits[3]];
-        }
+      output << tensPlace[digits[2] - 2];
+      if (digits[3] > 0) {
+        output << " " << lessThan20[digits[3]];
+      }
     }
+  }
 
-    std::cout << output.str() << std::endl;
+  std::cout << output.str() << std::endl;
 }
